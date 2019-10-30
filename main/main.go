b801778ae69d73139198c3fef311b7803368c89c
BaymaxRice <506489196@qq.com>
2019-10-30 15:27:47 +0800

    complete demo

--- main/main.go
+++ main/main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	ssr_demo "github.com/BaymaxRice/ssr-demo"
+)
+
+type CmdArgs struct {
+	isServer bool
+	//isClient bool
+	backRunning bool
+	conf        string
+}
+
+var cmdArgs CmdArgs
+
+func init() {
+	flag.BoolVar(&cmdArgs.isServer, "s", false, "输入-s：启动服务端程序")
+	//flag.BoolVar(&cmdArgs.isClient, "c", true, "输入-c：启动客户端程序")
+	flag.BoolVar(&cmdArgs.backRunning, "b", false, "输入-b：启动后台运行")
+	flag.StringVar(&cmdArgs.conf, "conf", "", "设置客户端或者服务端配置文件(默认取当前目录下的client.json)")
+
+	flag.Usage = usage
+	flag.Parse()
+}
+
+func usage() {
+	fmt.Println("usage:   ssr-demo  [-q] [-conf=<filepath>] [-s] [-b]")
+	flag.PrintDefaults()
+}
+
+func main() {
+	if cmdArgs.isServer {
+		server := ssr_demo.Server{}
+		err := server.LoadConf(cmdArgs.conf)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		_ = server.Run()
+	}
+
+	// 客户端程序
+	client := ssr_demo.Client{}
+	err := client.LoadConf(cmdArgs.conf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_ = client.Run()
+}
